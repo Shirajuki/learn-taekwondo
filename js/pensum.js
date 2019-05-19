8474813d6e4f156d1f4b55062f9eaffa9edcfedc
Shirajuki <vulturegfd@reborn.com>
2019-05-19 16:04:01 +0200

    first update finish cup 10

---
+++ js/pensum.js
@@ -0,0 +1,31 @@
+const cup10 = {
+  teori: [
+    {romanization: '', norsk: ''},
+    {romanization: 'Charyôt', norsk: 'Giv akt stilling'},
+    {romanization: 'Baro / Goman', norsk: 'Stopp'},
+    {romanization: 'Kyôngnye', norsk: 'Hilse'},
+    {romanization: 'Gihap (Ki-ap)', norsk: 'Skrik (Ki = Energi, Ap = Samle)'},
+    {romanization: 'Junbi', norsk: 'Start stilling'},
+    {romanization: 'Dwirodora', norsk: 'Snu'},
+  ],
+  spark: [
+    {romanization: '', norsk: '', video: ''},
+    {romanization: 'Ap chagi', norsk: 'Frontspark med vrist', video: 'yo'},
+    {romanization: 'Dollyo chagi',norsk: 'Rundspark', video: 'yo'},
+    {romanization: 'Yop chagi',norsk: 'Sidespark', video: 'yo'},
+  ],
+  monster: [
+    {romanization: '', video: ''},
+    {romanization: 'Taegeuk Il-jang', video: 'yo'},
+  ],
+  oneStep: [
+    {'': '', angrep: '', forsvar: ''},
+    {'': '1.', angrep: 'Dwit kobi area makki.<br/>Ap kobi area jireugi.<br/>', forsvar: '<br/>Dwit kobi area makki.<br/>Ap kobi momtong jireugi.'},
+    {'': '2.', angrep: 'Dwit kobi area makki.<br/>Ap kobi momtong jireugi.<br/>', forsvar: '<br/>Dwit kobi geodoreo makki.<br/>Ap kobi momtong jireugi.'},
+    {'': '3.', angrep: 'Dwit kobi oesantul makki.<br/>Ap kobi mejumeok olgul naeryo chigi.<br/>', forsvar: '<br/>Dwit kobi olgul makki.<br/>Ap kobi olgul jireugi.'},
+  ],
+  selvforsvar: [
+    {teknikk: '', video: ''},
+    {teknikk: 'Fallteknikker og rull', video: 'yo'},
+  ],
+};
